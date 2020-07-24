f04123a7c1fb1f2e884815364d225ccca1ad9bfb
KHanUshna <49242688+KHanUshna@users.noreply.github.com>
2020-07-23 21:00:51 -0400

    Add files via upload

---
+++ resource.js
@@ -0,0 +1,108 @@
+const BUTTON = [
+    {
+        text: '0',
+        id: 'zero',
+        className: 'button button-digit'
+    },
+    {
+        text: '1',
+        id: 'one',
+        className: 'button button-digit'
+    },
+    {
+        text: '2',
+        id: 'two',
+        className: 'button button-digit'
+    },
+    {
+        text: '3',
+        id: 'three',
+        className: 'button button-digit'
+    },
+    {
+        text: '4',
+        id: 'four',
+        className: 'button button-digit'
+    },
+    {
+        text: '5',
+        id: 'five',
+        className: 'button button-digit'
+    },
+    {
+        text: '6',
+        id: 'six',
+        className: 'button button-digit'
+    },
+    {
+        text: '7',
+        id: 'seven',
+        className: 'button button-digit'
+    },
+    {
+        text: '8',
+        id: 'eight',
+        className: 'button button-digit'
+    },
+    {
+        text: '9',
+        id: 'nine',
+        className: 'button button-digit'
+    },
+    {
+        text: '.',
+        id: 'decimal',
+        className: 'button button-digit'
+    },
+    {
+        text: '+',
+        id: 'add',
+        className: 'button button-operator'
+    },
+    {
+        text: '-',
+        id: 'subtract',
+        className: 'button button-operator'
+    },
+    {
+        text: 'x',
+        id: 'multiply',
+        className: 'button button-operator'
+    },
+    {
+        text: '÷',
+        id: 'divide',
+        className: 'button button-operator'
+    },
+    {
+        text: '=',
+        id: 'equals',
+        className: 'button button-operator'
+    },
+    {
+        text: 'C',
+        id: 'clear',
+        className: 'button button-cancel'
+    },
+];
+
+const CONTACT = [
+    {
+        description: 'LinkedIn',
+        font: 'fab fa-linkedin-in',
+        href: 'https://www.linkedin.com/in/isshwaryaa-sankar-80469a160/',
+        className: 'contact-link linkedin'
+    },
+    {
+        description: 'GitHub',
+        font: 'fab fa-github',
+        href: 'https://github.com/Isshwaryaa',
+        className: 'contact-link github'
+    },
+    {
+        description: 'freeCodeCamp',
+        font: 'fab fa-free-code-camp',
+        href: 'https://www.freecodecamp.org/learn',
+        className: 'contact-link freecodecamp'
+    }
+];
